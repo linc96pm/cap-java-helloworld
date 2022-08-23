87e14082175391dacf4871929f8dc3e47239fdf6
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2022-08-23 12:09:00 +0500

    first commit

---
+++ srv/src/main/java/customer/helloworld/handlers/SayHandler.java
@@ -0,0 +1,21 @@
+package customer.helloworld.handlers;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import cds.gen.say.Say_;
+import cds.gen.say.HelloContext;
+
+@Component
+@ServiceName(Say_.CDS_NAME)
+public class SayHandler implements EventHandler {
+  
+  @On(event = HelloContext.CDS_NAME)
+  public void sayHello(HelloContext context) {
+    String name = context.getTo();
+    String greeting = "Hello " + name + "!";
+    context.setResult(greeting);
+  }
+}
